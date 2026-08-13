295d71e039871a368e05767b8a0cfae3e268d856
DanaShaynan <danashinan2020@gmail.com>
2026-08-14 02:48:05 +0300

    Update solution.cpp

--- Task-01/solution.cpp
+++ Task-01/solution.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+
+    double mark1, mark2, mark3;
+    double average;
+    char grade;
+
+    cout << "Enter the mark for subject 1: ";
+    cin >> mark1;
+
+    cout << "Enter the mark for subject 2: ";
+    cin >> mark2;
+
+    cout << "Enter the mark for subject 3: ";
+    cin >> mark3;
+
+    average = (mark1 + mark2 + mark3) / 3;
+
+    if (average >= 90)
+        grade = 'A';
+    else if (average >= 80)
+        grade = 'B';
+    else if (average >= 70)
+        grade = 'C';
+    else if (average >= 60)
+        grade = 'D';
+    else
+        grade = 'F';
+
+    cout << "\nAverage: " << average << endl;
+    cout << "Grade: " << grade << endl;
+
+    return 0;
+}
